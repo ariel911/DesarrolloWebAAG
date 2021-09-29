65ea6e816ca05ba5d4a939dee7933e39f17ec460
Ariel911 <achu.ariel.gabriel@gmail.com>
2021-09-29 17:17:37 -0400

    subiendo trabajos en clase

--- avance/tema3/fibonacci/fibonacci.php
+++ avance/tema3/fibonacci/fibonacci.php
@@ -6,9 +6,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
 <body>
-    
+<?php
+   $num1=1;
+   $num2=0;
+   $n=10;
+   $s=0;
+   
+   for ($i=0;$i<=$n;$i++ ){
+        echo $s.",";
+       $s=$num1+$num2;
+       $num1 = $num2;
+       $num2 = $s;
+   }
+?>
+
+
+
+
 </body>
 </html>
 
